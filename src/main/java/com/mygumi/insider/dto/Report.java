3c8da9a3ab7e6f2a0b3ca7c644a7265a16cd0105
김정은 <kjenotn@gmail.com>
2023-07-11 16:02:05 +0900

    dto 수정

--- src/main/java/com/mygumi/insider/dto/Report.java
+++ src/main/java/com/mygumi/insider/dto/Report.java
@@ -3,6 +3,7 @@ package com.mygumi.insider.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 public class Report {
 
     @Id
