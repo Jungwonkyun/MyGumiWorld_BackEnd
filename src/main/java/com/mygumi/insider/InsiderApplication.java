6ef2b8356835ffa32d42d48834d8c5da6477da5c
김민수 <mskim.spring@gmail.com>
2023-07-12 22:07:14 +0900

    .

--- src/main/java/com/mygumi/insider/InsiderApplication.java
+++ src/main/java/com/mygumi/insider/InsiderApplication.java
@@ -2,6 +2,8 @@ package com.mygumi.insider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
